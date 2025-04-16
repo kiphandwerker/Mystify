60b38a8f83eb519574c84ec23c0731b97eda34d0
JKH <jhandwer@stjude.org>
2025-04-16 11:23:39 -0500

    Updated to resemble Usage in README.mp

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-import Mystify
+# Import the function
+import Mystify      
 
+# Initilize the function
 M = Mystify.Mystify()
-MainData = M.GenerateData(100)
-synthetic_data = M.Mystify(MainData, preserve_columns=['patient_id'])
-synthetic_data = M.Mystify(MainData)
-print(MainData)
-print(synthetic_data)
 
-M.SaveCSV(synthetic_data, "test")
+# Create a fake dataset with 100 people
+MainData = M.GenerateData(100)
+
+# Mystify the data
+synthetic_data = M.MystifyData(MainData)
+
+# Mystify the data but preserve patient_id
+synthetic_data = M.MystifyData(MainData, preserve_columns=['patient_id'])
+
+# Save for future exploration
+M.SaveCSV(synthetic_data, "MystifiedData")
