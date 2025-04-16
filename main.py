85598fcfd2a01860e0b5f996f85f91dff12809ab
JKH <jhandwer@stjude.org>
2025-04-16 10:44:58 -0500

    Updated with new functionality

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import Mystify
 
-df = Mystify.GenerateData(100)
-synthetic_data = Mystify.Mystify(df)
-#print(synthetic_data)
-print(synthetic_data.head())
-Mystify.SaveCSV(df,"test")
+M = Mystify.Mystify()
+MainData = M.GenerateData(100)
+synthetic_data = M.Mystify(MainData, preserve_columns=['patient_id'])
+synthetic_data = M.Mystify(MainData)
+print(MainData)
+print(synthetic_data)
+
+M.SaveCSV(synthetic_data, "test")
