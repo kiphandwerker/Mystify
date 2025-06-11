5fb08c82473acb7e601bca2d8ea50193f5a12dfe
JKH <jhandwer@stjude.org>
2025-06-11 12:05:54 -0500

    Added ShowPHIvars() and GenerateData with list of more variables

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ import Mystify
 # Initilize the function
 M = Mystify.Mystify()
 
-# Create a fake dataset with 100 people
+# Create a fake dataset with 100 people with defaults values
 MainData = M.GenerateData(100)
+
+# Get a list of all the PHI variables available
+M.ShowPHIvars()
+
+# Include specific variables
+MainData = M.GenerateData(n=100, MoreFields=['email', 'phone', 'insurance_id', 'diagnosis'])
 
 # Mystify the data
 synthetic_data = M.MystifyData(MainData)
